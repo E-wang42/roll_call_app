53c9102bc172d621c0313918dc8ec7dda5781422
eric <ericwang42@outlook.com>
2022-10-22 00:05:54 -0700

    update search useRef

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import { useState, useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import "./Search.css";
 
 function Search() {
-  // const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
+  const inputRef = useRef(null);
 
   // function focusInput() {
   //   const ref = useRef(null);
 
   //   useEffect(() => {
   //     ref.current.focus();
-  //   });
+  //   }, []);
   // }
 
-  // function clear() {
-  //   if (!value) {
-  //     setValue((prevValue) => (prevValue = ""));
-  //   }
-  //   return;
-  // }
+  function inputEvent(e) {
+    setValue(e.target.value);
+  }
+
+  function clearInput() {
+    inputRef.current.value = "";
+  }
 
   return (
     <div className="container__input">
@@ -26,11 +27,14 @@ function Search() {
         className="container__input--field"
         type="search"
         placeholder="Search Cats"
-        // value={value}
+        value={value}
         name="text"
-        // ref={focusInput}
+        onChange={inputEvent}
+        ref={inputRef}
       />
-      <button className="input__button">&times;</button>
+      <button onClick={clearInput} className="input__button">
+        &times;
+      </button>
     </div>
   );
 }
